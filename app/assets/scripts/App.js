9b886cccffa1f5ee7cb4af82c23303653f97f186
Maryanos <maryando78@gmail.com>
2023-03-04 12:39:27 +0000

    Fixed backgrounds and wrapped animation in forEach loops

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,24 +1,20 @@
 /* 3rd party */
 import "../styles/style.css"
-import 'lazysizes'
-
+import "lazysizes"
 
 /* Logics */
 import "./modules/Overlay"
 import "./modules/StickyHeader"
 
-
 /* Modules */
 import "./modules/SiteLogo"
 import "./modules/ContactForm"
 import "./modules/SiteFooter"
-
+import "./modules/Lottie"
 
 /* HRM */
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
-
-
 
 
